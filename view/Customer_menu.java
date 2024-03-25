cae2fa5a234b76f1af580437fa93e05789d8e8fe
oktavianusw <waluyajuang330@gmail.com>
2024-03-25 23:40:42 +0700

    Fix Login & Customer Menu

--- view/Customer_menu.java
+++ view/Customer_menu.java
@@ -4,15 +4,11 @@ import java.util.Scanner;
 
 import model.Customer;
 import model.User;
-import java.util.Scanner;
 
 public class Customer_menu {
 
     public static Scanner input = new Scanner(System.in);
-<<<<<<< HEAD
 
-=======
->>>>>>> 19401fb97722fe652a8ede639fe509ed69084c7e
     public static void customer_menu(Customer cust){
         System.out.println("========================");
         System.out.println("\tCustomer Menu");
@@ -22,7 +18,6 @@ public class Customer_menu {
         System.out.println("3. View Transaction");
         System.out.println("4. Wallet");
         System.out.println("0. Logout");
-<<<<<<< HEAD
 
         System.out.println("Choose : ");
         switch (input.nextInt()) {
@@ -44,21 +39,6 @@ public class Customer_menu {
             default:
                 System.out.println("Invalid input");
                 customer_menu(cust);
-=======
-        switch(input.nextInt()){
-            case 1:
-                Wallet_menu.main_menu(cust);
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-
-                break;
-            case 0:
-                Main_menu.main_menu();
->>>>>>> 19401fb97722fe652a8ede639fe509ed69084c7e
                 break;
         }
     }
