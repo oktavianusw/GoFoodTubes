f2c37371708a0e2f27c5baf6d6981d13d94aae50
juanSunitsuga <juankurniawan04@gmail.com>
2024-04-25 02:06:08 +0700

    Rp -> $

--- controller/Login_controller.java
+++ controller/Login_controller.java
@@ -13,8 +13,6 @@ public class Login_controller {
     public boolean login(String email, String password, UserType userType) throws IOException {
         if (userType.equals(UserType.CUSTOMER)) {
             for (Customer cust : Data_customer.cust) {
-                // System.out.println("Data : "+cust.getEmail() + " " + cust.getPassword());
-                // System.out.println("Input : "+email + " " + password);
                 if (cust.getEmail().equals(email) && cust.getPassword().equals(password)) {
                     System.out.println();
                     System.out.println("Login successful. Welcome, " + cust.getUsername() + "!");
@@ -25,8 +23,6 @@ public class Login_controller {
             }
         } else if (userType.equals(UserType.SELLER)){
             for (Seller seller : Data_seller.sellers) {
-                // System.out.println("Data : "+seller.getEmail() + " " + seller.getPassword());
-                // System.out.println("Input : "+email + " " + password);
                 if (seller.getEmail().equals(email) && seller.getPassword().equals(password)) {
                     System.out.println();
                     System.out.println("Login successful. Welcome, " + seller.getUsername() + "!");
